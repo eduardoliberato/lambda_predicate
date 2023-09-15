a0c062d17866b07b85121d2e1838d06ac7159bce
eduardoliberato <eduardo.d.liberato@gmail.com>
2023-09-14 21:52:57 -0300

    first commit

---
+++ predicate/application/application.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class application {
+    public static void main(String[] args) {
+        
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for(Product p : list){
+            System.out.println(p);
+        }
+
+
+
+    }
+}
